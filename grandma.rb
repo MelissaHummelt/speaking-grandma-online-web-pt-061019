35511bae2e71958383ba07b9503deffb9ff4e327
Melissa Hummelt <mkhummelt@gmail.com>
2019-06-15 18:14:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if phrase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "I LOVE YOU TOO PUMPKIN!"
